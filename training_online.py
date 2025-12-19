fc0349bce952d4cf30116126bd669c08088ba13e
syafiq <syafiqmuda98@gmail.com>
2025-12-19 19:31:58 +0800

    update

--- training_online.py
+++ training_online.py
@@ -1,4 +1,8 @@
+import os
 from ultralytics import YOLO, checks, hub
+
+os.environ['ULTRALYTICS_CACHE_DIR'] = '/opt/dlami/nvme/.ultralytics'
+
 checks()
 
 hub.login('40815389e96288255fd1121f17b108fbf9cedc7421')
