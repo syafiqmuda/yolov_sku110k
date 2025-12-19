3d073a80ce25366ab4245b537c52fe7e5ce8e67b
syafiq <syafiqmuda98@gmail.com>
2025-12-19 18:28:16 +0800

    init 2

--- training_online.py
+++ training_online.py
@@ -4,6 +4,5 @@ checks()
 
 if __name__ == "__main__":
     hub.login('40815389e96288255fd1121f17b108fbf9cedc7421')
-    
-    model = YOLO('https://hub.ultralytics.com/models/RDctHGy0iVk7Df4d5Ikc')
+    model = YOLO('https://hub.ultralytics.com/models/aMnqOTD7cqVO1RJ2s1tC')
     results = model.train()
