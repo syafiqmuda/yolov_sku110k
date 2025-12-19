a55dcbde1bd931a8963217f154d4a97989e8ef67
syafiq <syafiqmuda98@gmail.com>
2025-12-19 18:53:14 +0800

    update

--- training_online.py
+++ training_online.py
@@ -3,5 +3,5 @@ checks()
 
 hub.login('40815389e96288255fd1121f17b108fbf9cedc7421')
 
-model = YOLO('yolo11s.pt')
+model = YOLO('https://hub.ultralytics.com/models/aMnqOTD7cqVO1RJ2s1tC')
 results = model.train()
