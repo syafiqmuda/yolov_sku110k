12ee5487b49b6ba76c0dc9c2f282ab6cd0f800e1
syafiq <syafiqmuda98@gmail.com>
2025-12-19 18:42:50 +0800

    remove

--- training_online.py
+++ training_online.py
@@ -2,7 +2,6 @@ from ultralytics import YOLO, checks, hub
 
 checks()
 
-if __name__ == "__main__":
-    hub.login('40815389e96288255fd1121f17b108fbf9cedc7421')
-    model = YOLO('https://hub.ultralytics.com/models/aMnqOTD7cqVO1RJ2s1tC')
-    results = model.train()
+hub.login('40815389e96288255fd1121f17b108fbf9cedc7421')
+model = YOLO('https://hub.ultralytics.com/models/aMnqOTD7cqVO1RJ2s1tC')
+results = model.train()
