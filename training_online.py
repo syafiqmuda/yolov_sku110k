0434b7ba129c44bd6d892a58dac05d0ae7fdea1b
syafiq <syafiqmuda98@gmail.com>
2025-12-19 18:44:23 +0800

    update code

--- training_online.py
+++ training_online.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO, checks, hub
-
 checks()
 
 hub.login('40815389e96288255fd1121f17b108fbf9cedc7421')
+
 model = YOLO('https://hub.ultralytics.com/models/aMnqOTD7cqVO1RJ2s1tC')
 results = model.train()
