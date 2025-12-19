8ca5d8fcdb65d25711aa72da49be9c9d6d92bff9
syafiq <syafiqmuda98@gmail.com>
2025-12-19 19:08:09 +0800

    update

--- training_online.py
+++ training_online.py
@@ -1,6 +1,12 @@
-from ultralytics import YOLO, checks, hub
 import os
+from ultralytics import hub, checks
+
+# Step 1: Use NVMe for downloads and cache
 os.environ['ULTRALYTICS_CACHE_DIR'] = '/opt/dlami/nvme/ultralytics_cache'
+os.makedirs(os.environ['ULTRALYTICS_CACHE_DIR'], exist_ok=True)
+
+# Step 2: Optional: work in NVMe directory
+os.chdir('/opt/dlami/nvme/yolov_sku110k')
 
 checks()
 
